0018a01e543b2b13cc44d35b9646feb0585db171
Henrincode <spellctro@gmail.com>
2025-07-03 12:11:52 -0300

    sistema concluido

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 const notas = document.querySelectorAll(".nota");
+const campoTotal = document.querySelector(".rodape p");
 
+// Atualiza valores individuais e total ao digitar
 notas.forEach(nota => {
-  const hiddenInput = nota.querySelector('input[type="hidden"]');
-  const valorNota = Number(hiddenInput.value);
+  const inputQuantidade = nota.querySelector('input[type="number"]');
+  const valorUnitario = Number(nota.querySelector('input[type="hidden"]').value);
+  const campoResultado = nota.querySelector("p");
 
-  const input = nota.querySelector('input[type="number"]');
-  const total = nota.querySelector("p");
+  inputQuantidade.addEventListener("input", () => {
+    if(inputQuantidade.value < 0) inputQuantidade.value = 0
 
-  input.addEventListener("input", () => {
-    somar(input.value, total, valorNota);
+    const quantidade = Number(inputQuantidade.value);
+    const totalNota = quantidade * valorUnitario;
+    
+    campoResultado.innerHTML = formatarReal(totalNota);
+
+    atualizarTotalGeral();
   });
 });
 
-function somar(valor, texto, multiplicador) {
-  const resultado = Number(valor) * multiplicador;
-  texto.innerHTML = `R$${resultado.toFixed(2).replace(".",",")}`;
+// Formata número em R$
+function formatarReal(valor) {
+  return `R$${valor.toFixed(2).replace(".", ",")}`;
+}
+
+// Soma todos os valores totais
+function atualizarTotalGeral() {
+  let totalGeral = 0;
+
+  notas.forEach(nota => {
+    const quantidade = Number(nota.querySelector('input[type="number"]').value);
+    const valor = Number(nota.querySelector('input[type="hidden"]').value);
+    totalGeral += quantidade * valor;
+  });
+
+  campoTotal.innerHTML = `Valor total em caixa ${formatarReal(totalGeral)}`;
 }
